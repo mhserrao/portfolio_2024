79d91613f76a5accc5e8e216409a065efe5e3736
Maiziel Harriet Serrao <maizy_serrao@outlook.com>
2024-06-10 16:40:22 -0400

    Updated favicon and logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import nairobi from './backgrounds/nairobi.png';
 import whiteabstract from './backgrounds/white-abstract.png';
 import toronto from './backgrounds/toronto.png';
 
-import logo from './logo/logo-black.png';
+import logo from './logo/logo.png';
 import logotext from './logo/logo-text-black.png';
 
 import shaq from './personal/shaq.png';
