b4804009c117d724689814067eae156146d07349
mhserrao <maizy_serrao@outlook.com>
2025-06-14 21:49:10 -0400

    Updated links to work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import { fadeIn, textVariant } from '../utils/motion';
-import { SectionWrapper } from '../hoc';
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
     <motion.div
-      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
-      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
+    >
       <div
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
         <h3 className="text-taupe text-[18px] font-bold text-center">
           {title}
@@ -35,12 +37,15 @@ const About = () => {
       </motion.div>
 
       <motion.p
-        variants={fadeIn('', '', 0.1, 1)}
-        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
-        I've been in the software industry for a couple years now. All my projects are my art as I use logic in a creative way
-        to build innovative solutions for complex and dynamic problems. I absolutely love sinking my teeth into software projects, 
-        especially those that make a tangible difference in the world. Below, you can see some examples of the work I created.
-        Please contact me if you would like to chat further!
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]"
+      >
+        I've been in the software industry for a couple years now. All my
+        projects are my art as I use logic in a creative way to build innovative
+        solutions for complex and dynamic problems. I absolutely love sinking my
+        teeth into software projects, especially those that make a tangible
+        difference in the world. Below, you can see some examples of the work I
+        created. Please contact me if you would like to chat further!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -52,4 +57,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, 'about-wrapper');
+export default SectionWrapper(About, "about");
